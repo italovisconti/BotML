d372f5b36a219bdf7b261c613d4de4c0d47be302
Italo Visconti <hermanosvisconti@hotmail.com>
2023-02-20 22:06:51 -0400

    act

--- BotPrueba.py
+++ BotPrueba.py
@@ -2,13 +2,13 @@ from time import sleep
 from requests_html import HTMLSession
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
 url = "https://www.pccomponentes.com/asus-geforce-gtx-1050-tis-4gb-gddr5"
 url_coolmod = "https://www.pccomponentes.com/asus-geforce-gtx-1050-tis-4gb-gddr5"
 url_coolmod = "https://articulo.mercadolibre.com.ve/MLV-718947014-carro-de-perros-caliente-y-hamburguesas-_JM#position=6&search_layout=stack&type=item&tracking_id=99e36af4-229a-42f8-88e4-5cca200a0bd7"
 
 session = HTMLSession()
-
 
 def aviso_facil():
     while True:
@@ -50,8 +50,7 @@ def iniciar_sesion(driver):
         except NoSuchElementException:
             print("Puessss no esta el formulario...")"""
 
-    email = form.find_element_by_name("user_id")
-
+    email = driver.find_element(By.ID, value="user_id")
     email.send_keys("Prueba@hotmail.com")
     sleep(2)
 
@@ -63,15 +62,14 @@ def iniciar_compra(found):
     if len(found):
         driver = webdriver.Firefox()
         driver.get(url_coolmod)
-        sleep(7)
+        sleep(3)
         print("Funcionando")
         """Debemos cerrar el Banner de Cookies"""
-        driver.find_element(by="class name", value="cookie-consent-banner-opt-out__action.cookie-consent-banner-opt-out__action--primary.cookie-consent-banner-opt-out__action--key-accept").click()
-        driver.find_element(by="class name", value="ui-pdp-actions").click()
-        sleep(4)
-        driver.find_element(
-            by="class name", value="andes-button__content").click()
-        sleep(4)
+        driver.find_element(By.CLASS_NAME, value="cookie-consent-banner-opt-out__action.cookie-consent-banner-opt-out__action--primary.cookie-consent-banner-opt-out__action--key-accept").click()
+        driver.find_element(By.CLASS_NAME, value="ui-pdp-actions").click()
+        sleep(2)
+        driver.find_element(By.CLASS_NAME, value="login-link.andes-button--large.andes-button--transparent").click()
+        sleep(1)
 
         iniciar_sesion(driver)
 
